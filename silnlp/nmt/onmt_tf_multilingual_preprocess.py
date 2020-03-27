0d2e036ec5b616d0997717e0f8b8680650d2780f
Damien Daspit <damien_daspit@sil.org>
2020-03-27 18:03:46 +0700

    Use fixed test and validation sizes for each language pair

--- silnlp/nmt/onmt_tf_multilingual_preprocess.py
+++ silnlp/nmt/onmt_tf_multilingual_preprocess.py
@@ -13,7 +13,17 @@ from nlp.common.environment import paratextPreprocessedDir
 
 
 def get_parallel_corpus(
-    src_file_path: str, src_sentences: List[str], trg_file_path: str, trg_sentences: List[str], write_trg_token: bool
+    src_file_path: str,
+    trg_file_path: str,
+    train_src_sentences: List[str],
+    train_trg_sentences: List[str],
+    test_src_sentences: List[str],
+    test_trg_sentences: List[str],
+    val_src_sentences: List[str],
+    val_trg_sentences: List[str],
+    write_trg_token: bool,
+    test_size: int,
+    val_size: int,
 ) -> None:
     src_iso = get_iso(src_file_path)
     trg_iso = get_iso(trg_file_path)
@@ -21,6 +31,8 @@ def get_parallel_corpus(
     if src_iso == trg_iso:
         return
 
+    train_src: List[str] = list()
+    train_trg: List[str] = list()
     with open(src_file_path, "r", encoding="utf-8") as src_file, open(trg_file_path, "r", encoding="utf-8") as trg_file:
         for src_line, trg_line in zip(src_file, trg_file):
             src_line = src_line.strip()
@@ -29,8 +41,22 @@ def get_parallel_corpus(
                 continue
             if write_trg_token:
                 src_line = f"<2{trg_iso}> " + src_line
-            src_sentences.append(src_line)
-            trg_sentences.append(trg_line)
+            train_src.append(src_line)
+            train_trg.append(trg_line)
+    train_src, test_src, train_trg, test_trg = train_test_split(
+        train_src, train_trg, test_size=test_size, random_state=111
+    )
+    train_src, val_src, train_trg, val_trg = train_test_split(
+        train_src, train_trg, test_size=val_size, random_state=111
+    )
+    train_src_sentences.extend(train_src)
+    train_trg_sentences.extend(train_trg)
+
+    test_src_sentences.extend(test_src)
+    test_trg_sentences.extend(test_trg)
+
+    val_src_sentences.extend(val_src)
+    val_trg_sentences.extend(val_trg)
 
 
 def write_corpus(corpus_path: str, sentences: Iterable[str]) -> None:
@@ -62,6 +88,9 @@ def main() -> None:
     parser.add_argument("--task", required=True, help="Task name")
     parser.add_argument("--src", nargs="+", metavar="lang", help="Source language")
     parser.add_argument("--trg", nargs="+", metavar="lang", help="Target language")
+    parser.add_argument("--test-size", type=int, default=250, help="Test size for a language pair")
+    parser.add_argument("--val-size", type=int, default=250, help="Validation size for a language pair")
+    parser.add_argument("--vocab-size", type=int, default=24000, help="Shared vocabulary size")
     args = parser.parse_args()
 
     name = args.task
@@ -89,7 +118,8 @@ def main() -> None:
 
     sp_train_params = (
         f"--normalization_rule_name=nmt_nfkc_cf --input={joined_file_paths} --model_prefix={model_prefix}"
-        " --vocab_size=24000 --character_coverage=1.0 --input_sentence_size=1000000 --shuffle_input_sentence=true"
+        f" --vocab_size={args.vocab_size} --character_coverage=1.0 --input_sentence_size=1000000"
+        " --shuffle_input_sentence=true"
     )
 
     if write_trg_token:
@@ -109,19 +139,27 @@ def main() -> None:
     spp = sp.SentencePieceProcessor()
     spp.load(f"{model_prefix}.model")
 
-    src_sentences: List[str] = list()
-    trg_sentences: List[str] = list()
+    train_src_sentences: List[str] = list()
+    train_trg_sentences: List[str] = list()
+    test_src_sentences: List[str] = list()
+    test_trg_sentences: List[str] = list()
+    val_src_sentences: List[str] = list()
+    val_trg_sentences: List[str] = list()
     for src_file_path in src_file_paths:
         for trg_file_path in trg_file_paths:
-            get_parallel_corpus(src_file_path, src_sentences, trg_file_path, trg_sentences, write_trg_token)
-
-    train_src_sentences, test_src_sentences, train_trg_sentences, test_trg_sentences = train_test_split(
-        src_sentences, trg_sentences, test_size=2000, random_state=111
-    )
-
-    train_src_sentences, val_src_sentences, train_trg_sentences, val_trg_sentences = train_test_split(
-        train_src_sentences, train_trg_sentences, test_size=2000, random_state=111
-    )
+            get_parallel_corpus(
+                src_file_path,
+                trg_file_path,
+                train_src_sentences,
+                train_trg_sentences,
+                test_src_sentences,
+                test_trg_sentences,
+                val_src_sentences,
+                val_trg_sentences,
+                write_trg_token,
+                args.test_size,
+                args.val_size,
+            )
 
     write_corpus(os.path.join(root_dir, "train.src.txt"), tokenize_sentences(spp, train_src_sentences))
     write_corpus(os.path.join(root_dir, "train.trg.txt"), tokenize_sentences(spp, train_trg_sentences))
