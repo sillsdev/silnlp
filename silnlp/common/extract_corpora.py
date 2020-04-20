854fde001bd644789f6e01d5129215682d9a5c45
Michael Martin <michael_martin@sil.org>
2020-04-20 13:47:13 -0400

    Expose --include/--exclude options for extraction

--- silnlp/common/extract_corpora.py
+++ silnlp/common/extract_corpora.py
@@ -27,32 +27,43 @@ def get_iso(project_dir: str) -> Optional[str]:
     return iso[:index]
 
 
-def extract_corpus(output_dir: str, iso: str, project_dir: str) -> None:
+def extract_corpus(output_dir: str, iso: str, project_dir: str, include_texts: str, exclude_texts: str) -> None:
     name = os.path.basename(project_dir)
     print("Extracting", name, f"({iso})")
     ref_dir = os.path.join(paratextUnzippedDir, "Ref")
-    subprocess.run(
-        [
-            "translator",
-            "extract",
-            "-s",
-            f"pt,{ref_dir}",
-            "-t",
-            f"pt,{project_dir}",
-            "-to",
-            os.path.join(output_dir, f"{iso}-{name}.txt"),
-            "-st",
-            "null",
-            "-tt",
-            "null",
-            "-as",
-        ]
-    )
+    arg_list = [
+        "translator",
+        "extract",
+        "-s",
+        f"pt,{ref_dir}",
+        "-t",
+        f"pt,{project_dir}",
+        "-to",
+        os.path.join(output_dir, f"{iso}-{name}.txt"),
+        "-st",
+        "null",
+        "-tt",
+        "null",
+        "-as",
+    ]
+    if len(include_texts) > 0:
+        arg_list.append("-i")
+        arg_list.append(include_texts)
+    if len(exclude_texts) > 0:
+        arg_list.append("-e")
+        arg_list.append(exclude_texts)
+    subprocess.run(arg_list)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Extracts text corpora from Paratext projects")
     parser.add_argument("projects", nargs="*", metavar="name", help="Paratext project")
+    parser.add_argument(
+        "--include", metavar="texts", default="", help="The texts to include; e.g., '*NT*', '*OT*', 'GEN,EXO'"
+    )
+    parser.add_argument(
+        "--exclude", metavar="texts", default="", help="The texts to exclude; e.g., '*NT*', '*OT*', 'GEN,EXO'"
+    )
     args = parser.parse_args()
 
     projects = set(args.projects)
@@ -65,7 +76,7 @@ def main() -> None:
     for path in os.listdir(paratextUnzippedDir):
         if path == "Ref" or (len(projects) > 0 and path not in projects):
             continue
-        else :
+        else:
             projects_found.append(path)
 
     # Process the projects that have data and tell the user.
@@ -77,15 +88,16 @@ def main() -> None:
             if os.path.isdir(project_dir):
                 iso = get_iso(project_dir)
                 if iso is not None:
-                    extract_corpus(output_dir, iso, project_dir)
+                    extract_corpus(output_dir, iso, project_dir, args.include, args.exclude)
                     print(f"Processed: {project_dir}\nOutput saved in: {output_dir}")
-    else :
+    else:
         print(f"Couldn't find any data to process for any project.")
 
     # Tell the user which projects couldn't be found.
     for project in projects:
         if project not in projects_found:
             print(f"Couldn't find project {project}")
+
 
 if __name__ == "__main__":
     main()
