749551b5d6ebbfdae672fc06eb99482fea30c632
Damien Daspit <damien_daspit@sil.org>
2020-07-30 16:02:01 +0700

    Save results of SMT testing to a file

--- silnlp/smt/test.py
+++ silnlp/smt/test.py
@@ -6,6 +6,12 @@ import sacrebleu
 
 from nlp.common.corpus import load_corpus
 from nlp.common.utils import get_git_revision_hash, get_root_dir
+from nlp.smt.config import load_config
+
+
+def get_iso(lang: str) -> str:
+    index = lang.find("-")
+    return lang[:index]
 
 
 def main() -> None:
@@ -17,6 +23,9 @@ def main() -> None:
 
     exp_name = args.experiment
     root_dir = get_root_dir(exp_name)
+    config = load_config(exp_name)
+    src_iso = get_iso(config["src_lang"])
+    trg_iso = get_iso(config["trg_lang"])
 
     ref_file_path = os.path.join(root_dir, "test.trg.txt")
     predictions_file_path = os.path.join(root_dir, "test.trg-predictions.txt")
@@ -50,7 +59,17 @@ def main() -> None:
             del ref[i]
 
     bleu = sacrebleu.corpus_bleu(sys, [ref], lowercase=True)
-    print(bleu)
+    with open(os.path.join(root_dir, "bleu.csv"), "w", encoding="utf-8") as bleu_file:
+        bleu_file.write("src_iso,trg_iso,BLEU,1-gram,2-gram,3-gram,4-gram,BP,hyp_len,ref_len,sent_len\n")
+        bleu_file.write(
+            f"{src_iso},{trg_iso},{bleu.score:.2f},{bleu.precisions[0]:.2f},{bleu.precisions[1]:.2f},"
+            f"{bleu.precisions[2]:.2f},{bleu.precisions[3]:.2f},{bleu.bp:.3f},{bleu.sys_len:d},{bleu.ref_len:d},"
+            f"{len(sys):d}\n"
+        )
+    print(
+        f"{src_iso} -> {trg_iso}: {bleu.score:.2f} {bleu.precisions[0]:.2f}/{bleu.precisions[1]:.2f}"
+        f"/{bleu.precisions[2]:.2f}/{bleu.precisions[3]:.2f}/{bleu.bp:.3f}"
+    )
 
 
 if __name__ == "__main__":
