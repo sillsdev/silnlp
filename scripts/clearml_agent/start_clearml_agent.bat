1288433d64fa641393135afacfb46f4be60c83aa
John Lambert <john_lambert@sil.org>
2021-05-19 16:11:03 -0400

    Update clearml agent comments.

--- scripts/clearml_agent/start_clearml_agent.bat
+++ scripts/clearml_agent/start_clearml_agent.bat
@@ -1,7 +1,6 @@
-rem call  "mount_aqua_on_data.bat" before this file is called.
 rem move clearml.conf to C:/Users/Username/clearml.conf
 rem you can test the mounting in docker by executing the following:
-rem docker run -it -v \\wsl$\Ubuntu\data:/data silintlai/machine-silnlp:master-latest ls /data/aqua-ml-data
+rem docker run -it -v "G:\Shared drives\AQUA":/data/aqua-ml-data silintlai/machine-silnlp:master-latest ls /data/aqua-ml-data
 rem see if the folders in aqua S3 bucket are displayed.
 call clearml-agent daemon --cpu-only --queue cpu_only --docker
 pause
