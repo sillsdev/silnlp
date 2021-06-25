e82f760896bf3dce1c40daf9601a357370895bd3
John Lambert <john_lambert@sil.org>
2021-06-25 12:16:12 -0400

    #56 - add more logging when environment is chosen

--- silnlp/common/environment.py
+++ silnlp/common/environment.py
@@ -1,9 +1,12 @@
 import os
+import logging
 from pathlib import Path
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+LOGGER = logging.getLogger(__name__)
 
 ROOT_DIR = Path.home() / ".silnlp"
 
@@ -15,20 +18,29 @@ def get_data_dir() -> Path:
     if sil_nlp_data_path is not None:
         temp_path = Path(sil_nlp_data_path)
         if temp_path.is_dir():
+            LOGGER.info(f"Using workspace: {sil_nlp_data_path} as per environment variable SIL_NLP_DATA_PATH.")
             return Path(sil_nlp_data_path)
         else:
             raise Exception(
-                "The path defined by environment variable SIL_NLP_DATA_PATH ("
-                + sil_nlp_data_path
-                + ") is not a directory."
+                f"The path defined by environment variable SIL_NLP_DATA_PATH ({sil_nlp_data_path}) is not a directory."
             )
     auqa_ml_path = Path("G:/Shared drives/AQUA")
     if auqa_ml_path.is_dir():
+        LOGGER.info(
+            f"Using workspace: {auqa_ml_path}.  To change the workspace, set the environment variable SIL_NLP_DATA_PATH."
+        )
         return auqa_ml_path
     gutenberg_path = Path("G:/Shared drives/Gutenberg")
     if gutenberg_path.is_dir():
+        LOGGER.info(
+            f"Using workspace: {gutenberg_path}.  To change the workspace, set the environment variable SIL_NLP_DATA_PATH."
+        )
         return gutenberg_path
-    return ROOT_DIR / "data"
+    data_dir = ROOT_DIR / "data"
+    LOGGER.info(
+        f"Using workspace: {data_dir}.  To change the workspace, set the environment variable SIL_NLP_DATA_PATH."
+    )
+    return data_dir
 
 
 # Root data directory
