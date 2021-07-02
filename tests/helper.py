75daf6b4f74ed9fce7c1df82a71d1ebb7bc79538
john lambert <johnml1135@gmail.com>
2021-07-02 14:55:00 -0400

    #16 - update test checks to allow for small differences in sentencepiece.

--- tests/helper.py
+++ tests/helper.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 import gc
 import logging
 
@@ -43,15 +42,35 @@ def compare_folders(truth_folder: str, computed_folder: str):
                 assert tf_content[i] == cf_content[i], (
                     "Log entry line " + str(i) + " should be:\n  " + tf_content[i] + "\nbut is:\n  " + cf_content[i]
                 )
+        elif tf.endswith((".src.txt", ".trg.txt")):
+            tf_content = open(tfp, "r", encoding="utf-8").readlines()
+            cf_content = open(cfp, "r", encoding="utf-8").readlines()
+            for i in range(len(tf_content)):
+                # remove all whitespace, as it changes as per sentencepiece and is hard to control.
+                assert "".join(tf_content[i].split()) == "".join(
+                    cf_content[i].split()
+                ), f"line {i} in {tf} should be:\n  {tf_content[i]}\nbut is:\n  {cf_content[i]}"
         elif tf.endswith((".xml", ".txt", ".csv", ".json")):
             tf_content = open(tfp, "r", encoding="utf-8").readlines()
             cf_content = open(cfp, "r", encoding="utf-8").readlines()
             for i in range(len(tf_content)):
                 # normalize unix and PC endings
-                assert tf_content[i].strip() == cf_content[i].strip(), (
-                    "line " + str(i) + " in " + tf + " should be:\n  " + tf_content[i] + "\nbut is:\n  " + cf_content[i]
-                )
+                assert (
+                    tf_content[i].strip() == cf_content[i].strip()
+                ), f"line {i} in {tf} should be:\n  {tf_content[i]}\nbut is:\n  {cf_content[i]}"
+        elif tf.endswith((".vocab")):
+            # make sure it's not too different.
+            tf_set = set([l.split()[0] for l in open(tfp, "r", encoding="utf-8").readlines()])
+            cf_set = set([l.split()[0] for l in open(cfp, "r", encoding="utf-8").readlines()])
+            diff_set = (tf_set - cf_set) | (cf_set - tf_set)
+            # Sentencepiece may not always make it excaclty the same.  If there is a big change, flag it.
+            assert (
+                len(diff_set) < len(tf_set) * 0.01
+            ), f"There are {len(diff_set)} differences in the file {tf}.  They are: {diff_set}"
+        elif tf.endswith((".model")):
+            # there is no good way to compare models, other than that they are created.
+            continue
         else:
             tf_content = open(tfp, "rb").read()
             cf_content = open(cfp, "rb").read()
-            assert tf_content == cf_content, "The file " + tf + " was created differently."
+            assert tf_content == cf_content, f"The file {tf} was created differently."
