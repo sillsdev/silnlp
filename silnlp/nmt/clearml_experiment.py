7346c0b47fdbbdab2aa7f65f17f9ccc8ccd26ad2
john lambert <johnml1135@gmail.com>
2021-09-02 13:46:55 -0400

    We will be sharing GPU's

--- silnlp/nmt/clearml_experiment.py
+++ silnlp/nmt/clearml_experiment.py
@@ -72,7 +72,7 @@ def main() -> None:
         name=args.experiment,
         make_stats=True,  # limited by stats_max_size to process only Bibles
         mixed_precision=True,  # clearML GPU's can handle mixed precision
-        memory_growth=False,  # we can allocate all memory all the time
+        memory_growth=True,  # We will be sharing GPU's - let's not get greedy
         num_devices=-1,  # get all devices
     )
     exp.run()
