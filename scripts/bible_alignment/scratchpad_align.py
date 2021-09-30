6d7f74ad545fdc3061c35e772ccc37dd4814ee0d
john lambert <johnml1135@gmail.com>
2021-09-30 10:57:57 -0400

    The alignment script works in an elementary level.

--- scripts/bible_alignment/scratchpad_align.py
+++ scripts/bible_alignment/scratchpad_align.py
@@ -1,4 +1,5 @@
 import os
+import tempfile
 from pathlib import Path
 
 from silnlp.alignment.machine_aligner import FastAlignMachineAligner
@@ -7,31 +8,41 @@ from silnlp.alignment.utils import compute_alignment_score
 
 
 def align_set(src_input_path: Path, trg_input_path: Path, output_dir: Path):
+    if not src_input_path.exists():
+        raise FileExistsError(f"The source file does not exist:{src_input_path}")
+    if not trg_input_path.exists():
+        raise FileExistsError(f"The target file does not exist:{trg_input_path}")
     output_dir = Path(output_dir)
     output_dir.mkdir(exist_ok=True)
-    src_tok_output_path = output_dir / "tokenize-src-output.txt"
-    trg_tok_output_path = output_dir / "tokenize-trg-output.txt"
+    with tempfile.TemporaryDirectory() as td:
+        temp_dir = Path(td)
+        src_tok_output_path = temp_dir / "tokenize-src-output.txt"
+        trg_tok_output_path = temp_dir / "tokenize-trg-output.txt"
 
-    tokenize_corpus(src_input_path, src_tok_output_path)
-    tokenize_corpus(trg_input_path, trg_tok_output_path)
+        tokenize_corpus(src_input_path, src_tok_output_path)
+        tokenize_corpus(trg_input_path, trg_tok_output_path)
 
-    fast_align = FastAlignMachineAligner(output_dir)
+        fast_align = FastAlignMachineAligner(temp_dir)
 
-    sym_align_path = output_dir / "sym-align.txt"
-    fast_align.train(src_tok_output_path, trg_tok_output_path)
-    fast_align.align(sym_align_path)
+        sym_align_path = output_dir / "sym-align.txt"
+        fast_align.train(src_tok_output_path, trg_tok_output_path)
+        fast_align.align(sym_align_path)
 
-    direct_lexicon = fast_align.get_direct_lexicon(include_special_tokens=True)
-    inverse_lexicon = fast_align.get_inverse_lexicon(include_special_tokens=True)
+        direct_lexicon = fast_align.get_direct_lexicon(include_special_tokens=True)
+        inverse_lexicon = fast_align.get_inverse_lexicon(include_special_tokens=True)
 
     scores = []
     with src_tok_output_path.open("r", encoding="utf-8") as src_tok_output_file, trg_tok_output_path.open(
         "r", encoding="utf-8"
     ) as trg_tok_output_file, sym_align_path.open("r", encoding="utf-8") as sym_align_file:
         for src_sentence, trg_sentence, alignment in zip(src_tok_output_file, trg_tok_output_file, sym_align_file):
-            scores.append(
-                compute_alignment_score(direct_lexicon, inverse_lexicon, src_sentence, trg_sentence, alignment)
-            )
+            if src_sentence.strip() == "" or trg_sentence.strip() == "":
+                scores.append("N/A\n")
+            else:
+                scores.append(
+                    "%0.2f\n"
+                    % compute_alignment_score(direct_lexicon, inverse_lexicon, src_sentence, trg_sentence, alignment)
+                )
     with (output_dir / "alignment.scores.txt").open("w+", encoding="utf-8") as as_file:
         as_file.writelines(scores)
 
@@ -54,10 +65,10 @@ alignment_dir = scripture_dir / "..\\..\\alignments"
 # complete_files = full_bibles(scripture_dir)
 # (alignment_dir / "alignment_sources.txt").open('w+').writelines([f'{p.name}\n' for p in complete_files])
 complete_files = (alignment_dir / "alignment_sources.txt").open().readlines()
-trg_path = alignment_dir / "hbo-HEB.txt"
+src_path = alignment_dir / "hbo-HEB.txt"
 for f in complete_files[:3]:
-    filename = os.path.split(f)[1]
+    filename = os.path.split(f.strip())[1]
     name = os.path.splitext(filename)[0]
     f_dir = alignment_dir / name
     f_dir.mkdir(exist_ok=True)
-    align_set(scripture_dir / filename, trg_path, f_dir)
+    align_set(src_path, scripture_dir / filename, f_dir)
