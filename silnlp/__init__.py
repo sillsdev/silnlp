2b93a5339095f8bb8a919e5c5ab5a911650686a4
john lambert <johnml1135@gmail.com>
2021-10-19 14:25:25 -0400

    #83 - address double logging in tensorflow.

--- silnlp/__init__.py
+++ silnlp/__init__.py
@@ -7,3 +7,5 @@ ch = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 ch.setFormatter(formatter)
 LOGGER.addHandler(ch)
+# This is to prevent double logging on tensorflow: https://stackoverflow.com/questions/33662648/tensorflow-causes-logging-messages-to-double
+LOGGER.propagate = False
