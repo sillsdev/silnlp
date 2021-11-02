d08f818b999dcf418506a9feb290c84000f379bc
john lambert <johnml1135@gmail.com>
2021-11-02 15:16:48 -0400

    This file actually isn't needed.  clearml sessions are started from the command line instead.

--- scripts/blank_remote_task.py
+++
@@ -1,8 +0,0 @@
-from clearml import Task
-from silnlp.common.utils import get_git_revision_hash
-
-task = Task.init(project_name="LangTech_Interactive", task_name=get_git_revision_hash())
-task.set_base_docker(
-    docker_cmd="silintlai/machine-silnlp:master-latest",
-)
-task.execute_remotely(queue_name="langtech_40gb")
