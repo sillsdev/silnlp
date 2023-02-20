efb7275657752dbd8e3756a4ed6ebb14439bf8ff
David Baines <david_baines@sil.org>
2023-02-20 14:04:22 +0000

    Print experiment paths

--- silnlp/nmt/experiment.py
+++ silnlp/nmt/experiment.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from dataclasses import dataclass
 from pathlib import Path
+from pprint import pprint
 from typing import Optional
 
 from ..common.environment import SIL_NLP_ENV
@@ -77,6 +78,21 @@ class SILExperiment:
             self.name, patterns=("scores-*.csv", "test.*trg-predictions.*"), overwrite=True
         )
 
+from types import FunctionType
+from inspect import getmembers
+
+def api(obj):
+    return [name for name in dir(obj) if name[0] != '_']
+
+def attrs(obj):
+    disallowed_properties = {
+        name for name, value in getmembers(type(obj)) 
+        if isinstance(value, (property, FunctionType))
+    }
+    return {
+        name: getattr(obj, name) for name in api(obj) 
+        if name not in disallowed_properties and hasattr(obj, name)
+    }
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Run experiment - preprocess, train, and test")
@@ -102,6 +118,8 @@ def main() -> None:
 
     if args.mt_dir is not None:
         SIL_NLP_ENV.set_machine_translation_dir(SIL_NLP_ENV.data_dir / args.mt_dir)
+    
+    pprint(attrs(SIL_NLP_ENV))
 
     if args.memory_growth:
         enable_memory_growth()
