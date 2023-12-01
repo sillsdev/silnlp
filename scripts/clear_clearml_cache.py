d40632a7ba7a23c26a5ba22d7c9499df0223e252
mshannon-sil <matthew_shannon@sil.org>
2023-12-01 01:40:09 -0500

    fix clear cache script

--- scripts/clear_clearml_cache.py
+++ scripts/clear_clearml_cache.py
@@ -1,29 +1,28 @@
 import glob
 import os
+import shutil
+from pathlib import Path
 
-from clearml import Task
+from silnlp.nmt.clearml_connection import SILClearML
 
-# Warning!  This does not work - it needs to be further debugged.
-
-task = Task.init(
-    project_name="clear_cache",
-    task_name="clear_cache",
-)
-
-task.execute_remotely(queue_name="production")
+SILClearML("utilities/clear_cache", "production")
 
 
 def remove_files(path):
     print("deleting " + path)
     files = glob.glob(path + "/*")
     for f in files:
-        os.remove(f)
+        if Path.is_dir(Path(f)):
+            shutil.rmtree(f)
+        else:
+            os.remove(f)
 
 
-# remove_files("/root/.cache/huggingface")
-# remove_files("/var/cache/apt/archives")
+# shutil.rmtree("/root/.cache/huggingface")
+# shutil.rmtree("/var/cache/apt/archives")
 remove_files("/root/.cache/pip")
 remove_files("/root/.cache/pypoetry")
 remove_files("/root/.clearml/pip-download-cache")
-# remove_files("/clearml_agent_cache")
-# remove_files("/root/.clearml/venvs-cache")
+# shutil.rmtree("/clearml_agent_cache")
+# shutil.rmtree("/root/.clearml/venvs-cache")
+# shutil.rmtree("/root/.cache/vcs-cache")
