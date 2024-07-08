9600fc2cc092a53ccfe08c50438c5600930bd50d
Enkidu93 <lowryec17@gcc.edu>
2024-07-08 17:39:34 -0400

    Fix missing backslash

--- silnlp/common/environment.py
+++ silnlp/common/environment.py
@@ -175,7 +175,7 @@ class SilNlpEnv:
         if not self.is_bucket:
             return
         name = str(name)
-        pt_projects_path = str(self.data_dir / "Paratext" / "projects") if os.environ.get('SIL_NLP_PT_DIR', None) is not None else str(self.pt_dir.relative_to(self.data_dir) / "projects") + "/"
+        pt_projects_path = str(self.data_dir / "Paratext" / "projects") + "/" if os.environ.get('SIL_NLP_PT_DIR', None) is not None else str(self.pt_dir.relative_to(self.data_dir) / "projects") + "/"
         name = name.split(pt_projects_path)[-1]
         if len(name) == 0:
             raise Exception(
