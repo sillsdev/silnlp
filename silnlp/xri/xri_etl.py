a508b678bdcaeccf2bd80f757358713d987f1e37
Rohan M <rm@sil.org>
2024-08-22 18:11:23 +1000

    Add logic for saving .all.txt files to a unique output directory

--- silnlp/xri/xri_etl.py
+++ silnlp/xri/xri_etl.py
@@ -3,15 +3,33 @@ Transforms tsv files in the XRI format to extract files suitable for training SI
 
 Each execution of this script creates a unique directory within the output directory containing the output files.
 
+Example:
+
+    $ python xri_etl.py   data.tsv   swa     ngq     XRI-2024-08-14
+                             ^        ^       ^           ^
+                           input   source    target     dataset
+                           file    iso code  iso code   descriptor
+
+This creates a unique directory in `out` based on the script inputs and current time, and puts the files in there:
+
+    out
+    └── swa-ngq-XRI-2024-08-14-20240822-180428
+        ├── swa-XRI-2024-08-14.all.txt     <--- complete source extract file
+        └── ngq-XRI-2024-08-14.all.txt     <--- complete target extract file
+
 Run with --help for more details.
 
 See https://github.com/sillsdev/silnlp/issues/472 for original context.
 """
+
 import argparse
 import csv
+import os
+import time
 
 from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
 from typing import List
 
 
@@ -71,13 +89,37 @@ def load_sentence_pairs(input_file_path: str) -> List[SentencePair]:
     ]
 
 
-def create_extract_files(sentence_pairs: List[SentencePair]) -> None:
-    return
+def build_output_path(unique_output_dir: str, iso: str, dataset_descriptor: str, suffix: str) -> str:
+    return os.path.join("out", unique_output_dir, f"{iso}-{dataset_descriptor}.{suffix}.txt")
+
+
+def write_output_file(filename: str, sentences: List[str]) -> None:
+    with open(filename, "w", encoding="utf-8") as f:
+        for sentence in sentences:
+            f.write(f"{sentence}{os.linesep}")
+
+
+def create_extract_files(cli_input: CliInput, sentence_pairs: List[SentencePair]) -> None:
+    unique_output_dir = (
+        f"{cli_input.source_iso}-{cli_input.target_iso}-{cli_input.dataset_descriptor}-{time.strftime('%Y%m%d-%H%M%S')}"
+    )
+    Path(os.path.join("out", unique_output_dir)).mkdir(parents=True, exist_ok=True)
+
+    # *.all.txt files
+    source_filename = build_output_path(unique_output_dir, cli_input.source_iso, cli_input.dataset_descriptor, "all")
+    source_sentences = [sentence_pair.source for sentence_pair in sentence_pairs]
+    write_output_file(source_filename, source_sentences)
+
+    target_filename = build_output_path(unique_output_dir, cli_input.target_iso, cli_input.dataset_descriptor, "all")
+    target_sentences = [sentence_pair.target for sentence_pair in sentence_pairs]
+    write_output_file(target_filename, target_sentences)
+
+    # TODO add *.(train/dev/test).txt files in later PR
 
 
 def run(cli_input: CliInput) -> None:
     sentence_pairs = load_sentence_pairs(cli_input.input_file_path)
-    create_extract_files(sentence_pairs)
+    create_extract_files(cli_input, sentence_pairs)
 
 
 def main() -> None:
