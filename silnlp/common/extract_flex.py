c144199789b81d0324af9b8e2b2d940a57f7e43e
Enkidu93 <lowryec17@gcc.edu>
2024-08-28 10:02:54 -0400

    Add filtering logic

--- silnlp/common/extract_flex.py
+++ silnlp/common/extract_flex.py
@@ -6,32 +6,51 @@ from pathlib2 import Path
 from tqdm import tqdm
 
 
-def extract_flex(xml: ET.ElementTree, name: str, output_folder: Path, only_languages: "Optional[set[str]]" = None):
+def extract_flex(
+    xml: ET.ElementTree,
+    name: str,
+    output_folder: Path,
+    only_languages: "Optional[set[str]]" = None,
+    only_textids: "Optional[set[str]]" = None,
+):
     print("Extracting text corpora from FLEx data...")
-    all_phrase_elements = xml.findall("interlinear-text/paragraphs/paragraph/phrases/phrase")
     lines = 0
-    print(only_languages)
     text_per_lang: dict[str, str] = {}
-    for phrase_element in tqdm(all_phrase_elements):
-        items = phrase_element.findall("item")
-        languages_added = set()
-        for item in items:
-            if item.attrib["type"] in ["txt", "gls"]:
-                language_code = item.attrib["lang"]
-                if only_languages is not None and language_code not in only_languages:
-                    continue
-                languages_added.add(language_code)
-                if language_code not in text_per_lang:
-                    text_per_lang[language_code] = "\n" * (lines - 1)
-                text_per_lang[language_code] += (
-                    item.text.strip().strip("'").strip() + "\n" if item.text is not None else "\n"
-                )
+    text_ids_found = set()
+    text_elements = xml.findall("interlinear-text")
+    for text_element in tqdm(text_elements):
+        text_id_elements = list(filter(lambda i: i.attrib["type"] == "title", text_element.findall("item")))
+        if only_textids is not None and len(only_textids) > 0 and (len(text_id_elements) == 0 or not any(t.text in only_textids for t in text_id_elements)):
+            continue
+        if only_textids is not None and len(only_textids) > 0 and len(text_id_elements) > 0:
+            text_ids_found.add(text_id_elements[0].text)
+        phrase_elements = text_element.findall("paragraphs/paragraph/phrases/phrase")
+        for phrase_element in phrase_elements:
+            line_added = False
+            items = phrase_element.findall("item")
+            languages_added = set()
+            for item in items:
+                if item.attrib["type"] in ["txt", "gls"]:
+                    language_code = item.attrib["lang"]
+                    if only_languages is not None and len(only_languages) > 0 and language_code not in only_languages:
+                        continue
+                    line_added = True
+                    languages_added.add(language_code)
+                    if language_code not in text_per_lang:
+                        text_per_lang[language_code] = "\n" * lines
+                    text_per_lang[language_code] += (
+                        item.text.strip().strip("\u200e").strip("'").strip() + "\n" if item.text is not None else "\n"
+                    )
+            if line_added:
+                for language in set(text_per_lang.keys()) - languages_added:
+                    text_per_lang[language] += "\n"
                 lines += 1
-        for language in set(text_per_lang.keys()) - languages_added:
-            text_per_lang[language] += "\n"
     for language, text in text_per_lang.items():
         with open(output_folder / f"{language}-{name}.txt", "w") as f:
             f.write(text)
+    if only_textids is not None and len(only_textids) > 0 and len(only_textids) != len(text_ids_found):
+        print(f"Unable to find text id(s): {','.join(set(only_textids) - text_ids_found)}")
+
     print(f"Extracted {lines} lines of text")
 
 
@@ -48,6 +67,9 @@ def main():
     parser.add_argument(
         "--only-langs", required=False, nargs="+", default=set(), help="Only extract data for these language codes"
     )
+    parser.add_argument(
+        "--text-ids", required=False, nargs="+", default=set(), help="Only extract data from texts with these text ids"
+    )
     args = parser.parse_args()
     xml = None
     xml_path = Path(args.xml[0])
@@ -58,7 +80,11 @@ def main():
     if not output_folder.exists():
         raise ValueError(f"Output directory {str(output_folder)} must exist")
     extract_flex(
-        xml, xml_path.name.split(".")[0] if "." in xml_path.name else xml_path.name, output_folder, args.only_langs
+        xml,
+        xml_path.name.split(".")[0] if "." in xml_path.name else xml_path.name,
+        output_folder,
+        args.only_langs,
+        args.text_ids,
     )
 
 
