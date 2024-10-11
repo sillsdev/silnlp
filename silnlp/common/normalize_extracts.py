82260f225f4d4e3ace79dde63097a39a93c5d07f
Rohan M <rm@sil.org>
2024-10-11 17:06:44 +1100

    Issue 494: Add tests for normalization and fix many bugs

--- silnlp/common/normalize_extracts.py
+++ silnlp/common/normalize_extracts.py
@@ -39,73 +39,22 @@ You can change the logging level with the optional `--log-level LOG_LEVEL` which
 import argparse
 import logging
 import os
-import regex
 
 from dataclasses import dataclass
-from enum import Enum
 from glob import glob
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import List, Optional
+
+from .normalizer import (
+    logger as normalizer_logger,
+    Normalizer,
+    PunctuationCategory,
+    PunctuationNormalizationRule,
+    standard_normalizer,
+)
 
 logger = logging.getLogger(__package__ + ".normalize_extracts")
-all_loggers = [logger]  # More to be added
-
-
-class PunctuationCategory(Enum):
-    LEFT_CLINGING = "LEFT_CLINGING"
-    RIGHT_CLINGING = "RIGHT_CLINGING"
-    LEFT_RIGHT_CLINGING = "LEFT_RIGHT_CLINGING"
-    UNCLINGING = "UNCLINGING"
-
-
-@dataclass(frozen=True)
-class PunctuationNormalizationRule:
-    character: str  # length 1
-    category: PunctuationCategory
-
-
-@dataclass(frozen=True)
-class SentenceTransformation:
-    """
-    A representation of a delta applied to a sentence at a particular position
-    """
-
-    start_index: int
-    end_index: int
-    original: str
-    replacement: str
-    description: str
-
-
-class WarningCode(Enum):
-    multiple_punctuation = 0
-
-
-@dataclass(frozen=True)
-class NormalizationWarning:
-    """
-    A warning of a potential false negative/positive spotted during normalization
-    For example it may notice a character that looks like punctuation,
-    but it's not included in the list of punctuation characters specified by the user
-    """
-
-    start_index: int
-    end_index: int
-    warning_code: WarningCode
-    description: str
-
-
-@dataclass(frozen=True)
-class SentenceNormalizationSummary:
-    """
-    A summary of the normalization process for a particular sentence.
-    Warnings about potential issues are also included.
-    """
-
-    original_sentence: str
-    normalized_sentence: str
-    transformations: List[SentenceTransformation]
-    warnings: List[NormalizationWarning]
+all_loggers = [logger, normalizer_logger]
 
 
 @dataclass(frozen=True)
@@ -151,160 +100,6 @@ def normalized_path(output_dir: Path, input_path: Path) -> Path:
     return output_dir / output_filename
 
 
-class Normalizer:
-    """
-    Encapsulates the state required to normalize sentences
-    """
-
-    def __init__(self, punctuation_normalization_rules: List[PunctuationNormalizationRule]):
-        self.punctuation_normalization_rules = punctuation_normalization_rules
-
-        self.consecutive_spaces = regex.compile("\\s+")
-        escaped_punctuation_chars = "".join(regex.escape(rule.character) for rule in punctuation_normalization_rules)
-        self.punctuation_regex = regex.compile(f"[{escaped_punctuation_chars}\s]+")
-        # TODO - ensure no duplicate characters
-        # TODO - ensure strings are length 1
-        # TODO - ensure no whitespace
-
-    def normalize(self, sentence: str) -> SentenceNormalizationSummary:
-        """
-        Returns a normalized version of the input string.
-        """
-        logger.debug(f"Normalizing '{sentence}'")
-
-        # TODO - just here for debugging, remove when code is mature
-        def pretty_print_coords(start_index: int, end_index: int) -> None:
-            logger.debug(sentence)
-            logger.debug("012345678901234567890123456789")
-            logger.debug("0         1         2         ")
-            logger.debug(start_index * " " + len(text) * "*")
-            logger.debug(start_index * " " + f"({start_index},{end_index})")
-
-        punctuation_groups = ((match.group(), match.span()) for match in regex.finditer(self.punctuation_regex, sentence))
-
-        # Categorize each group found
-        consecutive_spaces_found: List[Tuple[str, int, int]] = []
-        single_punctuation_found: List[Tuple[str, int, int]] = []
-        multiple_punctuation_found: List[Tuple[str, int, int]] = []
-        for text, (start_index, end_index) in punctuation_groups:
-            if text == " ":
-                continue
-            pretty_print_coords(start_index, end_index)
-
-            whitespace_removed = regex.sub(self.consecutive_spaces, "", text)
-            # match is completely whitespace
-            if not whitespace_removed:
-                append_to = consecutive_spaces_found
-            # match has one punctuation character
-            elif len(whitespace_removed) == 1:
-                append_to = single_punctuation_found
-            # match has 2+ punctuation character
-            else:
-                append_to = multiple_punctuation_found
-
-            append_to.append((text, start_index, end_index))
-
-        consecutive_spaces_transformations = [
-            SentenceTransformation(start_index, end_index, text, " ", "Consecutive whitespace found")
-            for text, start_index, end_index in consecutive_spaces_found
-        ]
-        logger.debug(f"#consecutive space blocks   ={len(consecutive_spaces_found)}")
-        logger.debug(f"#single punctuation chunks  ={len(single_punctuation_found)}")
-        logger.debug(f"#multiple punctuation chunks={len(multiple_punctuation_found)}")
-
-        # TODO - probably move this method out
-        def normalize_single_punctuation_group(start_index: int, end_index: int, original_text: str) -> Optional[str]:
-            """
-            Normalizes the portion of text found that contains a single punctuation character
-            If the normalized text is the same as the original text, None is returned to indicate no replacement is needed
-            """
-            punctuation_char = regex.sub(self.consecutive_spaces, "", original_text)
-            punctuation_rule = next(
-                filter(lambda rule: rule.character == punctuation_char, self.punctuation_normalization_rules)
-            )
-            # TODO - add checking around the boundary
-            if punctuation_rule.category == PunctuationCategory.LEFT_CLINGING:
-                return " " + punctuation_char
-            elif punctuation_rule.category == PunctuationCategory.RIGHT_CLINGING:
-                return punctuation_char + " "
-            elif punctuation_rule.category == PunctuationCategory.LEFT_RIGHT_CLINGING:
-                # Figure out if it's left or right clinging
-                # TODO think about edge cases around the boundary
-                # TODO in the case of no surrounding punctuation this implementation is left biased
-                # TODO think about the case of space on both sides
-                if original_text[0] == punctuation_char:
-                    return " " + punctuation_char
-                elif original_text[-1] == punctuation_char:
-                    return " " + punctuation_char
-                else:
-                    # TODO - this needs to generate a warning
-                    # Probably pick this up one higher up
-                    # When those 3 lists are built, you could add more context
-                    # Or you could do a dirty side effect
-                    return None
-            elif punctuation_rule.category == PunctuationCategory.UNCLINGING:
-                return " " + punctuation_char + " "
-            else:
-                return None
-
-        single_punctuation_analysis = [
-            (start_index, end_index, text, normalize_single_punctuation_group(start_index, end_index, text))
-            for text, start_index, end_index in single_punctuation_found
-        ]
-
-        single_punctuation_transformations = [
-            SentenceTransformation(start_index, end_index, text, normalized, "Punctuation found")
-            for start_index, end_index, original, normalized in single_punctuation_analysis
-            if normalized is not None
-        ]
-
-        multiple_punctuation_warnings = [
-            NormalizationWarning(
-                start_index,
-                end_index,
-                WarningCode.multiple_punctuation,
-                f"Text {text} contains 2 or more punctuation characters - currently this is not normalized",
-            )
-            for text, start_index, end_index in multiple_punctuation_found
-        ]
-
-        # TODO - add other kinds of warnings
-        all_warnings = multiple_punctuation_warnings
-
-        all_transformations = sorted(
-            consecutive_spaces_transformations + single_punctuation_transformations,
-            key=lambda transformation: transformation.start_index,
-        )
-
-        # Rebuild the string by applying all the transformations
-        # We extract the parts unaffected by normalization, then rebuild by interleaving them with the normalized parts
-        # Example:
-        #   Hello , there you  !Bye
-        #   -----   ---------   ---     <-- original parts
-        #        ---         ---        <-- parts to normalize
-        #        ", "         "!"       <-- normalized replacements
-        parts = []
-        last_part_end_index = 0
-        for transformation in all_transformations:
-            # The part prior to this normalization segment
-            parts.append(sentence[last_part_end_index : transformation.start_index])
-            last_part_end_index = transformation.end_index
-            parts.append(transformation.replacement)
-        # Deal with the ending original part
-        parts.append(sentence[last_part_end_index:])
-
-        normalized = "".join(parts)
-        # TODO - test an example where there's no normalization
-        # TODO - shortcircuit this if no normalization? I guess it will be pretty fast
-
-        return SentenceNormalizationSummary(
-            original_sentence=sentence,
-            normalized_sentence=normalized,
-            transformations=all_transformations,
-            warnings=all_warnings,
-        )
-
-
 def load_extract_file(path: Path) -> List[str]:
     with open(path, "r", encoding="UTF-8") as file:
         return [line.rstrip() for line in file]
@@ -337,19 +132,6 @@ def run(cli_input: CliInput) -> None:
     files_to_normalize: List[Path] = get_files_to_normalize(input_dir, cli_input.filter)
     logger.info(f"Found {len(files_to_normalize)} files to normalize")
 
-    # TODO - replace with cli input
-    normalizer = Normalizer(
-        [
-            PunctuationNormalizationRule(".", PunctuationCategory.RIGHT_CLINGING),
-            PunctuationNormalizationRule(",", PunctuationCategory.RIGHT_CLINGING),
-            PunctuationNormalizationRule("?", PunctuationCategory.RIGHT_CLINGING),
-            PunctuationNormalizationRule("!", PunctuationCategory.RIGHT_CLINGING),
-        ]
-    )
-    # TODO - test with left clinging
-    # TODO - test with left/right clinging
-    # TODO - test with unclinging
-
     for input_path in files_to_normalize:
         logger.debug(f"Processing file {input_path}")
         output_path: Path = normalized_path(output_dir, input_path)
@@ -365,7 +147,8 @@ def run(cli_input: CliInput) -> None:
         logger.debug(f"Found {len(input_lines)} lines in file")
         normalized_lines: List[str] = [
             # TODO - process the summaries
-            normalizer.normalize(extract_sentence).normalized_sentence
+            # TODO - replace with cli input
+            standard_normalizer.normalize(extract_sentence).normalized_sentence
             for extract_sentence in input_lines
         ]
         write_extract_file(output_path, normalized_lines)
