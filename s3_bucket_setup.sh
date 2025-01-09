d2851fdf3aeb3577645b6983cbc99d787ac8761f
Ben King <benjaminking@sil.org>
2025-01-09 15:23:01 -0500

    Run rclone in the foreground to accomodate WSL

--- s3_bucket_setup.sh
+++ s3_bucket_setup.sh
@@ -1,8 +1,8 @@
-cp scripts/rclone/rclone.conf ~/.config/rclone
+cp /workspaces/silnlp/scripts/rclone/rclone.conf ~/.config/rclone
 
 sed -i -e "s#access_key_id = x*#access_key_id = $AWS_ACCESS_KEY_ID#" ~/.config/rclone/rclone.conf
 sed -i -e "s#secret_access_key = x*#secret_access_key = $AWS_SECRET_ACCESS_KEY#" ~/.config/rclone/rclone.conf
 
 echo "Mounting S3 bucket..."
-nohup rclone mount --vfs-cache-mode full --use-server-modtime s3silnlp:silnlp /silnlp 2>&1 > s3_bucket_log.txt &
+rclone mount --vfs-cache-mode full --use-server-modtime s3silnlp:silnlp /silnlp
 echo "Done"
