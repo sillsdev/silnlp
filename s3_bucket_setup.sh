a277dc22b84427bcbadb1f6afc53a8ed7a9b9e33
Ben King <ben.p.king@gmail.com>
2025-02-06 13:42:39 -0500

    Add apparmor profile and change mount point to ~/S

--- s3_bucket_setup.sh
+++ s3_bucket_setup.sh
@@ -4,5 +4,5 @@ sed -i -e "s#access_key_id = x*#access_key_id = $AWS_ACCESS_KEY_ID#" ~/.config/r
 sed -i -e "s#secret_access_key = x*#secret_access_key = $AWS_SECRET_ACCESS_KEY#" ~/.config/rclone/rclone.conf
 
 echo "Mounting S3 bucket..."
-nohup rclone mount --vfs-cache-mode full --use-server-modtime s3silnlp:silnlp /silnlp 2>&1 > s3_bucket_log.txt &
+rclone mount --vfs-cache-mode full --use-server-modtime s3silnlp:silnlp ~/S
 echo "Done"
