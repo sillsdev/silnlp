dd478503cf39490b5bacc3ab431c1d0e656f065b
Ben King <benjamin_king@sil.org>
2025-04-02 09:45:48 -0400

    Delete s3_bucket_setup.sh
    Remove old s3_bucket_setup.sh script.

--- s3_bucket_setup.sh
+++
@@ -1,9 +0,0 @@
-cp scripts/rclone/rclone.conf ~/.config/rclone
-
-sed -i -e "s#access_key_id = x*#access_key_id = $MINIO_ACCESS_KEY#" ~/.config/rclone/rclone.conf
-sed -i -e "s#secret_access_key = x*#secret_access_key = $MINIO_SECRET_KEY#" ~/.config/rclone/rclone.conf
-sed -i -e "s#endpoint = x*#endpoint = $MINIO_ENDPOINT_URL#" ~/.config/rclone/rclone.conf
-
-echo "Mounting MinIO bucket..."
-rclone mount --vfs-cache-mode full --use-server-modtime miniosilnlp:nlp-research ~/M
-echo "Done"
