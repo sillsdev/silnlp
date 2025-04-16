25d00c8da47f4f0cd6fd126f8e71308d5b53d97b
TaperChipmunk32 <matthewbeech737@gmail.com>
2025-04-16 12:06:14 -0500

    Fix bug in backblaze_bucket_setup.sh

--- backblaze_bucket_setup.sh
+++ backblaze_bucket_setup.sh
@@ -4,5 +4,5 @@ sed -i -e "s#account = x*#account = $B2_KEY_ID#" ~/.config/rclone/rclone.conf
 sed -i -e "s#key = x*#key = $B2_APPLICATION_KEY#" ~/.config/rclone/rclone.conf
 
 echo "Mounting Backblaze bucket..."
-rclone mount --daemon --log-file=/root/rclone_log.txt --log-level=DEBUG --vfs-cache-mode full --use-server-modtime miniosilnlp:nlp-research ~/B
+rclone mount --daemon --log-file=rclone_log.txt --log-level=DEBUG  --vfs-cache-mode full --use-server-modtime b2silnlp:silnlp ~/B
 echo "Done"
