731b4778ef5a6ec0fb3cbea2386c0c5af7aef55d
David Baines <david_baines@sil.org>
2026-02-05 15:06:23 +0000

    Split out the argument parsing for books and refactor check_books.py

--- silnlp/common/book_args.py
+++ silnlp/common/book_args.py
@@ -1,8 +1,39 @@
 from pathlib import Path
+import textwrap
+from machine.corpora import FileParatextProjectSettingsParser
 
 from .collect_verse_counts import DT_CANON, NT_CANON, OT_CANON
 VALID_CANONS = ["OT", "NT", "DT"]
-VALID_BOOKS = OT_CANON + NT_CANON + DT_CANON
+VALID_BOOKS = OT_CANON + NT_CANON + DT_CANON + ['FRT', 'BAK', 'OTH', 'INT']
+
+
+def add_books_argument(parser):
+    parser.add_argument(
+        "--books",
+        metavar="books",
+        nargs="*",
+        default=[],
+        help="Books to check or process (e.g., GEN EXO NT). Use NT, OT, DT for canons"
+    )
+
+
+def get_epilog():
+    return textwrap.dedent(
+         """
+             Books can include corpora NT OT or DT and individual books.
+             Old Testament books are :
+             GEN, EXO, LEV, NUM, DEU, JOS, JDG, RUT, 1SA, 2SA, 1KI, 2KI, 1CH, 2CH, EZR, NEH, EST, JOB, PSA, PRO, ECC,
+             SNG, ISA, JER, LAM, EZK, DAN, HOS, JOL, AMO, OBA, JON, MIC, NAM, HAB, ZEP, HAG, ZEC, MAL
+
+             New Testament books are :
+             MAT, MRK, LUK, JHN, ACT, ROM, 1CO, 2CO, GAL, EPH, PHP, COL, 1TH,
+             2TH, 1TI, 2TI, TIT, PHM, HEB, JAS, 1PE, 2PE, 1JN, 2JN, 3JN, JUD, REV
+
+             Deuterocanonical books are:
+             TOB, JDT, ESG, WIS, SIR, BAR, LJE, S3Y, SUS, BEL, 1MA,
+             2MA, 3MA, 4MA, 1ES, 2ES, MAN, PS2, ODA, PSS, EZA, JUB, ENO
+         """
+        )
 
 
 def expand_book_list(books):
@@ -20,25 +51,39 @@ def expand_book_list(books):
     return [book for book in VALID_BOOKS if book in set(books_to_check)]
 
 
-def get_sfm_files_to_process(settings, project_dir, specified_books):
-    sfm_suffix = Path(settings.file_name_suffix).suffix.lower()[1:]
-    # print(f"suffix is {sfm_suffix}")
+def get_sfm_files_to_process(project_dir, books):
 
-    # Find all SFM/USFM files
-    sfm_files = [
-        file
-        for file in project_dir.glob("*")
-        if file.is_file() and file.suffix[1:].lower() in ["sfm", "usfm", sfm_suffix]
-    ]
+    settings = FileParatextProjectSettingsParser(project_dir).parse()
+    sfm_files = []
+    for book in books:
+        sfm_file = Path(project_dir) / settings.get_book_file_name(book)
+        if sfm_file.is_file():
+            sfm_files.append(sfm_file)
 
-    # Parse books argument
-    if specified_books:
-        book_list = expand_book_list(specified_books)
+    return sfm_files
+    
+def group_bible_books(books_found):
+    ot_books = set(OT_CANON)
+    nt_books = set(NT_CANON)
+    dt_books = set(DT_CANON)
 
-        # Get book IDs for found files
-        ids_of_books_found = [settings.get_book_id(sfm_file.name) for sfm_file in sfm_files]
-        return [sfm_file for sfm_file in sfm_files if settings.get_book_id(sfm_file.name) in book_list]
+    books_set = set(books_found)
 
-    # No books are specified or filtered,  return all of them.
-    else:
-        return sfm_files
+    grouped_books = []
+
+    if ot_books.issubset(books_set):
+        grouped_books.append("OT")
+        books_set -= ot_books
+
+    if nt_books.issubset(books_set):
+        grouped_books.append("NT")
+        books_set -= nt_books
+
+    if dt_books.issubset(books_set):
+        grouped_books.append("DT")
+        books_set -= dt_books
+
+    # Add any remaining individual books
+    grouped_books.extend(sorted(books_set))
+
+    return grouped_books
--- silnlp/common/check_books.py
+++ silnlp/common/check_books.py
@@ -1,59 +1,26 @@
 import argparse
 import logging
+from math import exp
 import textwrap
 from pathlib import Path
 
 from machine.corpora import FileParatextProjectSettingsParser, UsfmFileText
 from machine.scripture import book_number_to_id, get_chapters
 
-from ..common.paratext import get_project_dir
-from .collect_verse_counts import DT_CANON, NT_CANON, OT_CANON
+from .paratext import get_project_dir
+from .book_args import DT_CANON, NT_CANON, OT_CANON, VALID_CANONS, VALID_BOOKS, get_epilog, group_bible_books, add_books_argument, expand_book_list, get_sfm_files_to_process
+
 
 LOGGER = logging.getLogger(__package__ + ".check_books")
 
-valid_canons = ["NT", "OT", "DT"]
-valid_books = []
-valid_books.extend(OT_CANON)
-valid_books.extend(NT_CANON)
-valid_books.extend(DT_CANON)
-
-
-def group_bible_books(books_found):
-    ot_books = set(OT_CANON)
-    nt_books = set(NT_CANON)
-    dt_books = set(DT_CANON)
-
-    books_set = set(books_found)
-
-    grouped_books = []
-
-    if ot_books.issubset(books_set):
-        grouped_books.append("OT")
-        books_set -= ot_books
-
-    if nt_books.issubset(books_set):
-        grouped_books.append("NT")
-        books_set -= nt_books
-
-    if dt_books.issubset(books_set):
-        grouped_books.append("DT")
-        books_set -= dt_books
-
-    # Add any remaining individual books
-    grouped_books.extend(sorted(books_set))
-
-    return grouped_books
-
-
-def parse_book(project_dir: str, book: str):
+def parse_book(project_dir: str, book_path: Path):
     errors = []
 
     settings = FileParatextProjectSettingsParser(project_dir).parse()
-    book_path = Path(project_dir) / settings.get_book_file_name(book)
-    LOGGER.info(f"Attempting to parse {book} from {book_path}.")
+    LOGGER.info(f"Attempting to parse {book_path}.")
     
     if not book_path.is_file():
-        raise RuntimeError(f"Can't find file {book_path} for book {book}")
+        raise RuntimeError(f"Can't find {book_path}")
 
     try:
         file_text = UsfmFileText(
@@ -70,9 +37,9 @@ def parse_book(project_dir: str, book: str):
         errors.append(e)
 
     if not errors:
-        LOGGER.info(f"{book} in file {book_path} parsed correctly and contains {file_text.count()} verses.")
+        LOGGER.info(f"{book_path} parsed correctly and contains {file_text.count()} verses.")
     else:
-        LOGGER.info(f"The following error occured while parsing {book} from file {book_path}.")
+        LOGGER.info(f"The following error occured while parsing {book_path}.")
         for error in errors:
             error_str = " ".join([str(s) for s in error.args])
             LOGGER.info(error_str)
@@ -84,76 +51,26 @@ def main() -> None:
         prog="check_books",
         description="Checks sfm files for a project with the same parser as translate.py",
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent(
-            """\
-             Books can include corpora NT OT or DT and individual books.
-             Old Testament books are :
-             GEN, EXO, LEV, NUM, DEU, JOS, JDG, RUT, 1SA, 2SA, 1KI, 2KI, 1CH, 2CH, EZR, NEH, EST, JOB, PSA, PRO, ECC,
-             SNG, ISA, JER, LAM, EZK, DAN, HOS, JOL, AMO, OBA, JON, MIC, NAM, HAB, ZEP, HAG, ZEC, MAL
-
-             New Testament books are :
-             MAT, MRK, LUK, JHN, ACT, ROM, 1CO, 2CO, GAL, EPH, PHP, COL, 1TH,
-             2TH, 1TI, 2TI, TIT, PHM, HEB, JAS, 1PE, 2PE, 1JN, 2JN, 3JN, JUD, REV
-
-             Deuterocanonical books are:
-             TOB, JDT, ESG, WIS, SIR, BAR, LJE, S3Y, SUS, BEL, 1MA,
-             2MA, 3MA, 4MA, 1ES, 2ES, MAN, PS2, ODA, PSS, EZA, JUB, ENO
-         """
-        ),
+        epilog=get_epilog(),
     )
 
     parser.add_argument("project", type=str, help="The name of the project folder.")
-    parser.add_argument(
-        "--books", metavar="books", nargs="+", default=[], help="The books to check; e.g., 'NT', 'OT', 'GEN EXO'"
-    )
+    add_books_argument(parser)  # adds the --books arg
 
     # parser.print_help()
     args = parser.parse_args()
-
     project_dir = get_project_dir(args.project)
-    settings = FileParatextProjectSettingsParser(project_dir).parse()
+    books = expand_book_list(args.books)
+    books = books if books else VALID_BOOKS
+    sfm_files = get_sfm_files_to_process(project_dir, books)
 
-    sfm_files = [
-        file for file in project_dir.glob("*") if file.is_file() and file.suffix[1:].lower() in ["sfm", "usfm"]
-    ]
-    books_found = [settings.get_book_id(sfm_file.name) for sfm_file in sfm_files]
-
-    grouped_result = group_bible_books(books_found)
-    LOGGER.info(f"Found these books in the project_directory: {' '.join(grouped_result)}")
-
-    if not sfm_files:
-        LOGGER.info(f"No sfm files found in project folder: {project_dir}")
-    else:
-        books = args.books
-        canons_to_add = [canon for canon in books if canon in valid_canons]
-
-        books_to_check = [book for book in books if book in books_found]
-
-        OT_books_found = [book for book in OT_CANON if book in books_found]
-        NT_books_found = [book for book in NT_CANON if book in books_found]
-        DT_books_found = [book for book in DT_CANON if book in books_found]
-
-        for canon_to_add in canons_to_add:
-            if canon_to_add == "OT":
-                books_to_check.extend(OT_books_found)
-            if canon_to_add == "NT":
-                books_to_check.extend(NT_books_found)
-            if canon_to_add == "DT":
-                books_to_check.extend(DT_books_found)
-
-        LOGGER.info(f"All books to check are: {books_to_check}\n")
-
-        if not books_to_check:
-            LOGGER.info("No books were specified, will check all books.")
-            books_to_check = books_found
-        else:
-            LOGGER.info(f"Of the books specified these were found: {books_to_check}")
-
-        book_nums = get_chapters(books_to_check)
-        book_nums_to_check = [book_number_to_id(book) for book in book_nums.keys()]
-
-        for book_num_to_check in book_nums_to_check:
-            parse_book(project_dir, book_num_to_check)
+    if not args.books:
+        LOGGER.info("No books were specified, will check all books.")
+        
+    LOGGER.info(f"Will check these books: {books}\n")
+  
+    for sfm_file in sfm_files:
+        parse_book(project_dir, sfm_file)
 
 
 if __name__ == "__main__":
