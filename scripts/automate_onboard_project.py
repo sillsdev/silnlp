772f66f3a5d05c7c03a6367164be12b3321363ae
Matthew Beech <matthewbeech737@gmail.com>
2026-02-09 13:49:33 -0600

    Fixed bugs

--- scripts/automate_onboard_project.py
+++ scripts/automate_onboard_project.py
@@ -1,15 +1,20 @@
 # Add a command line argument for the task name so that we can create a new task for each onboarding request and track them separately in ClearML
 import argparse
 import os
+from pathlib import Path
 
 from clearml import Task
 from clearml.backend_api.session.session import LoginError
 
-from silnlp.nmt.clearml_connection import set_base_docker_image
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--task-name", type=str, required=True, help="The name of the ClearML task to create for this onboarding request."
+)
+parser.add_argument(
+    "--dir",
+    type=str,
+    required=True,
+    help="The name of the directory in ONBOARDING_PATH to use as the source for onboarding.",
 )
 args = parser.parse_args()
 
@@ -20,19 +25,54 @@ try:
         tags=["silnlp-auto-onboarding"],
     )
 
-    task = set_base_docker_image(task)
+    task.set_base_docker(
+        docker_image="ghcr.io/sillsdev/silnlp:latest",
+        docker_arguments=[
+            "--env TOKENIZERS_PARALLELISM='false'",
+            "--cap-add SYS_ADMIN",
+            "--device /dev/fuse",
+            "--security-opt apparmor=docker-apparmor",
+            "--env CHECK_TRANSFERS=1",
+            "--env SIL_NLP_DATA_PATH=/root/M",
+            f"-v {os.getenv('ONBOARDING_PATH')}/{args.dir}:/root/OnboardingProjects/{args.dir}",
+        ],
+        docker_setup_bash_script=[
+            "apt install -y python3-venv",
+            "python3 -m pip install --user pipx",
+            "PATH=$PATH:/root/.local/bin",
+            "pipx install poetry==1.7.1",
+            # update config.toml and pyvenv.cfg to give poetry environment access to system site packages
+            "poetry config virtualenvs.options.system-site-packages true",
+            (
+                "sed -i 's/include-system-site-packages = .*/include-system-site-packages = true/' "
+                "/root/.local/share/pipx/venvs/poetry/pyvenv.cfg"
+            ),
+            # automatically connect to the MinIO bucket
+            "apt-get install --no-install-recommends -y fuse3 rclone",
+            "mkdir -p /root/M",
+            "mkdir -p /root/.config/rclone",
+            "cp scripts/rclone/rclone.conf /root/.config/rclone/",
+            'sed -i -e "s#access_key_id = x*#access_key_id = $MINIO_ACCESS_KEY#" ~/.config/rclone/rclone.conf',
+            'sed -i -e "s#secret_access_key = x*#secret_access_key = $MINIO_SECRET_KEY#" ~/.config/rclone/rclone.conf',
+            'sed -i -e "s#endpoint = .*#endpoint = $MINIO_ENDPOINT_URL#" ~/.config/rclone/rclone.conf',
+            "rclone mount --daemon --no-check-certificate --log-file=/root/rclone_log.txt --log-level=DEBUG --vfs-cache-mode full --use-server-modtime miniosilnlp:nlp-research /root/M",
+        ],
+    )
+
     task.execute_remotely(queue_name="jobs_backlog.cpu_only")
+
     import sys
 
     from silnlp.common import onboard_project
 
     old_argv = sys.argv
-    projects = os.listdir("/root/OnboardingProjects")
+    onboard_projects_dir = f"/root/OnboardingProjects/{args.dir}"
+    projects = os.listdir(onboard_projects_dir)
     try:
         sys.argv = [
             *projects,
             "--copy-from",
-            "/root/OnboardingProjects",
+            onboard_projects_dir,
             "--extract-corpora",
             "--wildebeest",
             "--extract-corpora",
