bb17bfca12622f80adcf122c894b5df5027cb250
Ben King <benjaminking@sil.org>
2026-03-13 19:56:35 +0000

    Use re-merged scripture refs for confidence files

--- silnlp/common/translator.py
+++ silnlp/common/translator.py
@@ -509,7 +509,9 @@ class Translator(AbstractContextManager["Translator"], ABC):
                     f.write(usfm_out)
 
                 if save_confidences and config.get_postprocess_suffix() == "":
-                    generate_confidence_files(translated_draft, trg_draft_file_path, scripture_refs=scripture_refs)
+                    generate_confidence_files(
+                        translated_draft, trg_draft_file_path, scripture_refs=translated_text_rows.get_scripture_refs()
+                    )
 
     def translate_docx(
         self,
